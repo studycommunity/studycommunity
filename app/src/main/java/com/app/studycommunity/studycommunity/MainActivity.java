822a8d816a4d1cd4e2cd8977426bb6c048115f3b
studycommunity2015 <studycommunity>
2015-12-29 22:37:48 +0800

    Initial commit

---
+++ app/src/main/java/com/app/studycommunity/studycommunity/MainActivity.java
@@ -0,0 +1,50 @@
+package com.app.studycommunity.studycommunity;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends ActionBarActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        //设置javasctipt可用
+        webView.getSettings().setJavaScriptEnabled(true);
+        //加载url，但是不会显示，注意哦！！
+        webView.loadUrl("http://www.baidu.com");
+        //指定显示控件(class)
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
